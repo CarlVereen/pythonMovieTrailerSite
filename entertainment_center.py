b5dbd720ef39a37c91ff604e19b8dee03a3f89c1
Carl Vereen <carlvereen@gmail.com>
2015-05-31 22:54:35 -0600

    added class variables and instance var

--- entertainment_center.py
+++ entertainment_center.py
@@ -4,33 +4,51 @@ import media
 ameadus = media.Movie("Amadeus",
                       "Mozart's short life, Did Salieri bring it to an end?",
                       "http://upload.wikimedia.org/wikipedia/en/2/2f/Amadeusmov.jpg",
-                      "https://www.youtube.com/watch?v=yIzhAKtEzY0")
+                      "https://www.youtube.com/watch?v=yIzhAKtEzY0",
+                      "Milos Forman",
+                      "1984",
+                      "R")
                       
 big_hero_six = media.Movie("Big Hero Six",
                            "A boy and his friendly medical robot",
                            "http://upload.wikimedia.org/wikipedia/en/4/4b/Big_Hero_6_%28film%29_poster.jpg",
-                           "http://www.youtube.com/watch?v=bT8qmoCgxZg")
+                           "http://www.youtube.com/watch?v=bT8qmoCgxZg",
+                           "Don Hall & Chris Williams",
+                           "2014",
+                           "PG")
 
 
 star_wars_new_hope = media.Movie("Star Wars, A New Hope",
                                  "The force brings a new hero to balance the powers in the universe",
                                  "http://upload.wikimedia.org/wikipedia/en/8/87/StarWarsMoviePoster1977.jpg",
-                                 "http://www.youtube.com/watch?v=1g3_CFmnU7k")
+                                 "http://www.youtube.com/watch?v=1g3_CFmnU7k",
+                                 "George Lucas",
+                                 "1977",
+                                 "PG")
 
 fearless = media.Movie("Fearless",
                        "A hero, a teacher, a lost soul",
                        "http://upload.wikimedia.org/wikipedia/en/4/4a/Fearless_film.jpg",
-                       "https://www.youtube.com/watch?v=23LxENZE8zo")
+                       "https://www.youtube.com/watch?v=23LxENZE8zo",
+                       "Ronny Yu",
+                       "2006",
+                       "PG-13")
 
 ong_bak = media.Movie("Ong-Bak",
                       "A village lost a precious treasure, A village hero will get it back",
                       "http://upload.wikimedia.org/wikipedia/en/1/11/ONG-BAK.jpg",
-                      "http://www.youtube.com/watch?v=nAYUv9wjV48")
+                      "http://www.youtube.com/watch?v=nAYUv9wjV48",
+                      "Prachya Pinkaew",
+                      "2003",
+                      "R")
 
 kungfu_hustle = media.Movie("Kung Fu Hustle",
                             "He always wanted to be a Kung Fu Master, little did he know he would be a Kung Fu Hero",
                             "http://upload.wikimedia.org/wikipedia/en/0/00/KungFuHustleHKposter.jpg",
-                            "http://www.youtube.com/watch?v=PRbPXbgsKyE")
+                            "http://www.youtube.com/watch?v=PRbPXbgsKyE",
+                            "Stephen Chow",
+                            "2004",
+                            "R")
 
 movies = [ameadus, big_hero_six, star_wars_new_hope, fearless, ong_bak, kungfu_hustle]
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,12 +1,34 @@
 import webbrowser
 
-class Movie():
-    def __init__ (self, movie_title, movie_storyline, poster_image, trailer_youtube) :
-        self.title = movie_title
-        self.storyline = movie_storyline
+#define a class that has the basic content for any type of video (ie: TV, Movie, youtube, home video etc...)
+class Video():
+    def __init__(self, video_title, video_storyline, poster_image, trailer_youtube):
+        self.title = video_title
+        self.storyline = video_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer (self) :
         webbrowser.open(self.trailer_youtube_url)
+
+#define a class that has the specific content for Movies and inherits basics of Video class
+class Movie(Video):
+    def __init__ (self, video_title, video_storyline, poster_image, trailer_youtube, director_name, release_date, movie_rating) :
+        Video.__init__(self, video_title, video_storyline,  poster_image, trailer_youtube)
+        self.director_name = director_name
+        self.release_date = release_date
+        self.movie_rating = movie_rating
+
+#define a class that has the specific conten for TV shows and inherits basic of Video class
+class TV(Video):
+    def __init__(self, video_title, video_storyline, poster_image, trailer_youtube, broadcast_station_name, pilot_date, number_of_seasons, tv_rating) :
+        Video.__init__(self, video_title, video_storyline,  poster_image, trailer_youtube)
+        self.broadcast_station_name = broadcast_station_name
+        self.pilot_date = pilot_date
+        self.number_of_seasons = number_of_seasons
+        self.tv_rating = tv_rating
+    
+        
+       
+
 
